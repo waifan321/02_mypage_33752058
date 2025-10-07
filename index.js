3ce5a0e4b669adfbc720fd96c0dbcfabe68af3cf
waifan321 <waileungfan@gmail.com>
2025-10-07 18:51:46 +0100

    small change

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 //Mypage web app 
 
 const http = require('http');
-
+const port = 3000;
 
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end(`
     <!DOCTYPE html>
-    <html>
-      <head>
-        <title>My Page</title>
-      </head>
-      <body>
-        <h1>My Page</h1>
-        <h2>About Me</h2>
-        <p>Hello! My name is Wai Fan. I like to gamble with my maintenance loan.</p>
-      </body>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>MyPage</title>
+    </head>
+    <body>
+      <h1>Hello World!</h1>
+      <h2>Welcome to My Page</h2>
+      <p>Hi, my name is Wai Fan. I like to gamble with my maintenance loan.</p>
+    </body>
     </html>
   `);
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('Server running at http://localhost:3000');
 });
