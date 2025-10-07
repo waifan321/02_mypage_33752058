4bdacfd3fc9814ee87d266d42a4f5978682bdbc4
waifan321 <waileungfan@gmail.com>
2025-10-07 17:51:25 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>My Page</title>
+      </head>
+      <body>
+        <h1>My Page</h1>
+        <h2>About Me</h2>
+        <p>Hello! My name is Wai Fan. I like to gamble with my maintenance loan.</p>
+      </body>
+    </html>
+  `);
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
